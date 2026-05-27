d45c3455bede8ede3fed0b8f508f743b45dfe7af
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-05-27 22:00:12 +0300

    feat: add portfolio chart types and historical mock data

---
+++ frontend/data/portfolio-mock.ts
@@ -0,0 +1,23 @@
+import { PortfolioHistory } from "../types/portfolio";
+
+export const MOCK_PORTFOLIO_HISTORY: PortfolioHistory = {
+  "7d": [
+    { date: "May 21", value: 1185000 },
+    { date: "May 22", value: 1210000 },
+    { date: "May 23", value: 1195000 },
+    { date: "May 24", value: 1230000 },
+    { date: "May 25", value: 1255000 },
+    { date: "May 26", value: 1240000 },
+    { date: "May 27", value: 1250430 },
+  ],
+  "30d": [
+    { date: "Apr 28", value: 950000 },
+    { date: "May 02", value: 980000 },
+    { date: "May 06", value: 1050000 },
+    { date: "May 10", value: 1020000 },
+    { date: "May 14", value: 1100000 },
+    { date: "May 18", value: 1150000 },
+    { date: "May 22", value: 1210000 },
+    { date: "May 27", value: 1250430 },
+  ],
+};
